5916a7c965c5b65d04388abb1607b5925f55dda4
Xinbo <xinboliu555@gamil.com>
2026-03-08 14:15:38 +0100

    Base Proyecto de Linkoria backend

---
+++ src/main/java/com/xinbo/springboot/backend/linkoria/app/SpringbootBackendLinkoriaApplication.java
@@ -0,0 +1,13 @@
+package com.xinbo.springboot.backend.linkoria.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBackendLinkoriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBackendLinkoriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xinbo/springboot/backend/linkoria/app/SpringbootBackendLinkoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xinbo.springboot.backend.linkoria.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBackendLinkoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

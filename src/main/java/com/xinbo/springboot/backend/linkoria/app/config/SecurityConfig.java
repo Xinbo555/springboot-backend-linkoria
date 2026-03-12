cd808af46e8663f91a6e5a82f9e82bdaceba7987
Xinbo <xinboliu555@gamil.com>
2026-03-12 16:23:18 +0100

    corregido bug en el que /error no estaba disponible para todos los usuarios

--- src/main/java/com/xinbo/springboot/backend/linkoria/app/config/SecurityConfig.java
+++ src/main/java/com/xinbo/springboot/backend/linkoria/app/config/SecurityConfig.java
@@ -40,6 +40,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.POST, "/auth/login", "/auth/register", "/auth/refresh").permitAll()
                         .requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs", "/v3/api-docs/**").permitAll()
+                        .requestMatchers("/error").permitAll()
                         .anyRequest().authenticated()
                 )
                 // Registra JwtFilter antes del filtro de autenticación estándar de Spring

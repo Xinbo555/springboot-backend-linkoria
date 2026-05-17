86ee3cfb552a3830145fedebc73358f5a74cd56d
Xinbo <xinboliu555@gamil.com>
2026-05-17 23:20:00 +0200

    hotfix, configuración async

---
+++ src/main/java/com/xinbo/springboot/backend/linkoria/app/config/AsyncConfig.java
@@ -0,0 +1,9 @@
+package com.xinbo.springboot.backend.linkoria.app.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+}
